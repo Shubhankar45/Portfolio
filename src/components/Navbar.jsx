2de3e038eee4527b2d972e01e5888fedc70a13d0
Shubhankar45 <shubhankarpatel2004@gmail.com>
2024-07-30 12:55:22 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+import {FaLinkedin} from 'react-icons/fa' ;
+import {FaGithub} from 'react-icons/fa' ;
+import {FaSquareXTwitter} from 'react-icons/fa6' ;
+import {FaInstagram} from 'react-icons/fa' ;
+import { motion } from "framer-motion"
+const Navbar = () => {
+  return (
+    <nav className="mb-10 flex item-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center ">
+            {/* <img className="mx-2 w-10" src={logo} alt="logo" /> */}
+            <span className="mx-2 w-10 text-3xl font-bold" >Shubh.</span>
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaLinkedin/>
+            <FaGithub/>
+            <FaSquareXTwitter/>
+            <FaInstagram/>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,46 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Hello! I'm  a curious and friendly individual with a passion for learning and growth. I enjoy exploring new ideas, trying new things, and meeting new people. I'm a creative problem solver, a good listener, and a supportive friend. I believe in being kind, empathetic, and understanding towards others. I'm always looking for new adventures, opportunities to learn, and ways to make a positive impact. I'm excited to connect with like-minded individuals and see where life takes us!I’m looking to collaborate on Full Stack Projects ,I’m looking for help with Projects. I'm excited to apply my skills and knowledge to real-world problems and continue learning and growing as a developer.`;
+
+export const ABOUT_TEXT = `Hi, I'm Shubhankar Patel, a passionate and driven web developer with a strong foundation in the MERN stack (MongoDB, Express.js, React.js, Node.js). With a solid understanding of programming fundamentals in Java and Python, I'm excited to bring my skills to the table and continue learning and growing as a developer..`;
+
+
+export const PROJECTS = [
+  {
+    title: "Music Player",
+    image: project1,
+    description:
+      "A simple music player that plays music ",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Food Delivery Website",
+    image: project2,
+    description:
+      "An application for ordering online food.Fully functional Food delivery app ",
+    technologies: ["MongoDB", "Expressjs", "Reactjs", "Nodejs "],
+  },
+  {
+    title: "MediCare",
+    image: project3,
+    description:
+      "Hospital Management Web app that check if a patient is corona positive or not and give him prescription",
+    technologies: ["MongoDB","Expressjs","Reactjs","Nodejs"],
+  },
+  {
+    title: "Vulnerability Scanner",
+    image: project4,
+    description:
+      "A platform for scanning ip and to fixed the vulnerable port ",
+    technologies: ["Java","Nessus","NMap"],
+  },
+];
+
+export const CONTACT = {
+  address: "Bhubaneswar,India",
+  phoneNo: "+917846863931 ",
+  email: "shubhankaarpatel2004@gmail.com",
+};
